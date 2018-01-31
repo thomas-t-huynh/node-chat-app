8f969c866806fceea0108aaacca7820c2598729b
unknown <t_huynh927@yahoo.com>
2018-01-30 23:59:07 -0800

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -37,11 +37,13 @@ socket.on('disconnect', function () {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    let messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-        
+        messageTextbox.val('');
     });
 });
 
@@ -51,12 +53,17 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+    
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.attr('disabled', 'disabled');
         alert('unable to fetch location');
     })
 });
