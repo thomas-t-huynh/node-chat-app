851c381d5ba35266742b0272960a41af6f2a1042
unknown <t_huynh927@yahoo.com>
2018-01-31 18:10:38 -0800

    added scroll feature if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -22,7 +39,7 @@ socket.on('newMessage', function( message ) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -37,7 +54,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
-    
+    scrollToBottom();
 })
   
 
