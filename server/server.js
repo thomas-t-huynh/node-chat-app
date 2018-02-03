338644e17be07702c384dd50f61adee7bc15e998
unknown <t_huynh927@yahoo.com>
2018-02-02 16:34:16 -0800

    made room names not case-sensitive

--- server/server.js
+++ server/server.js
@@ -25,28 +25,34 @@ io.on('connection', (socket) => {
 
 
     socket.on('join', (params , callback) => {
+
+        let room = params.room.toLowerCase();
+
         if (!isRealString(params.name) || !isRealString(params.room)) {
             return callback('Name and room name are required');
         }
 
-        socket.join(params.room.toLowerCase());
+        socket.join(room);
         users.removeUser(socket.id);
-        users.addUser(socket.id , params.name, params.room);
+        users.addUser(socket.id , params.name, room);
 
 
-        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        io.to(room).emit('updateUserList', users.getUserList(room));
         socket.emit('newMessage', generateMessage( 'admin', 'welcome to the chat app'));
 
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('admin', `${params.name} has joined.`));
+        socket.broadcast.to(room).emit('newMessage', generateMessage('admin', `${params.name} has joined.`));
         
         callback();
     })
 
     socket.on('createMessage', (message, callback) => {
+        
         let user = users.getUser(socket.id);
 
+        let room = user.room.toLowerCase();
+
         if (user && isRealString(message.text)) {
-             io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+             io.to(room).emit('newMessage', generateMessage(user.name, message.text));
         }
        
         callback();
@@ -56,8 +62,10 @@ io.on('connection', (socket) => {
 
         let user = users.getUser(socket.id);
 
+        let room = user.room.toLowerCase();
+
         if (user) {
-             io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude , coords.longitude))
+             io.to(room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude , coords.longitude))
         }
         
     })
@@ -66,9 +74,11 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         let user = users.removeUser(socket.id);
 
+        let room = user.room.toLowerCase();
+
         if (user) {
-            io.to(user.room).emit('updateUserList', users.getUserList(user.room));;
-            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+            io.to(room).emit('updateUserList', users.getUserList(room));;
+            io.to(room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
         }
         console.log('User disconnected');
     })
